fab848e20b49bd4e2eb45613a5fb5189761deae0
vzhyhunou <vicazh@tut.by>
2024-06-24 22:05:48 +0300

    merge with base

--- app/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFiles: ['./jest.setup.js'],
-    moduleNameMapper: {
-        '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/file.js',
-        '\\.(css|less)(|\\?.+)$': '<rootDir>/src/__mocks__/file.js'
-    }
-};
