65257bb4832f7d2a6de4089d9a9a02d683b7ff2f
vzhyhunou <vicazh@tut.by>
2024-08-27 22:03:20 +0300

    merge with base

--- client/src/provider/resources/products/index.js
+++ client/src/provider/resources/products/index.js
@@ -22,44 +22,26 @@ import product21 from './9/21.json';
 import product22 from './9/22.json';
 
 export default [
-    ...[
-        product1,
-        product2,
-        product5,
-        product6,
-        product7,
-        product8
-    ].map(p => ({...p, parents: [1]})),
-    ...[
-        product3,
-        product4
-    ].map(p => ({...p, parents: [2]})),
-    ...[
-        product9,
-        product10
-    ].map(p => ({...p, parents: [3]})),
-    ...[
-        product11,
-        product12
-    ].map(p => ({...p, parents: [4]})),
-    ...[
-        product13,
-        product14
-    ].map(p => ({...p, parents: [5]})),
-    ...[
-        product15,
-        product16
-    ].map(p => ({...p, parents: [6]})),
-    ...[
-        product17,
-        product18
-    ].map(p => ({...p, parents: [7]})),
-    ...[
-        product19,
-        product20
-    ].map(p => ({...p, parents: [8]})),
-    ...[
-        product21,
-        product22
-    ].map(p => ({...p, parents: [9]}))
+    product1,
+    product2,
+    product3,
+    product4,
+    product5,
+    product6,
+    product7,
+    product8,
+    product9,
+    product10,
+    product11,
+    product12,
+    product13,
+    product14,
+    product15,
+    product16,
+    product17,
+    product18,
+    product19,
+    product20,
+    product21,
+    product22
 ];
