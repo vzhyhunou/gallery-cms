fc355204aa4c3b07511f5f4fea590382dcf9f61b
vzhyhunou <vicazh@tut.by>
2024-10-17 21:51:00 +0300

    remove env props from bundle

--- client/src/config.js
+++ client/src/config.js
@@ -4,7 +4,7 @@ import { HashLink } from 'react-router-hash-link';
 import Catalog from './cms/resources/catalogs/App';
 import Menu from './cms/resources/menu/App';
 
-export default {
+export default ((REACT_APP_SRC, REACT_APP_BASE) => ({
     locales: {
         en: 'English',
         ru: 'Русский'
@@ -39,6 +39,6 @@ export default {
         Menu,
         HashLink
     },
-    provider: import(`./provider/${process.env.REACT_APP_SRC}.js`),
-    basename: process.env.REACT_APP_BASE
-};
+    provider: import(`./provider/${REACT_APP_SRC}.js`),
+    basename: REACT_APP_BASE
+}))(process.env.REACT_APP_SRC, process.env.REACT_APP_BASE);
